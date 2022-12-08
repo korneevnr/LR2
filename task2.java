8e899ee4e8d985ee4dd75ab0731778374a067513
korneevnr <114693945+korneevnr@users.noreply.github.com>
2022-12-08 18:49:18 +0300

    Create task2.java

---
+++ task2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Пароль: ");
+        Scanner scanner = new Scanner(System.in);
+        String date = scanner.next();
+        Matcher m = Pattern.compile("(?=\\S*[0-9])(?=\\S*[a-z])(?=\\S*[A-Z])\\S{8,}").matcher(date);
+        if(m.matches())
+            System.out.println("Пароль надежен");
+        else
+            System.out.println("Пароль ненадежен: попробуйте снова");
+    }
+}
