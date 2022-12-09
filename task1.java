acf3fe9caf55f3773d42631271351ae8e8831804
korneevnr <114693945+korneevnr@users.noreply.github.com>
2022-12-09 03:21:13 +0300

    Update task1.java

--- task1.java
+++ task1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+        import java.util.regex.Matcher;
+        import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,13 +8,32 @@ public class Main {
         System.out.println("Дата: ");
         String date = scanner.next();
         Matcher m = Pattern.compile("(0[1-9]|[12][0-9]|3[01])[/](0[1-9]|1[012])[/](19[9]\\d|[2-9][0-9]\\d\\d)").matcher(date);
-        Matcher d = Pattern.compile("(3[2-9]|[4-9][0-9])[/](1[3-9]|[2-9][0-9])[/](\\d\\d\\d\\d)").matcher(date);
         Matcher k = Pattern.compile("(\\d\\d)[/](\\d\\d)[/](\\d\\d\\d\\d)").matcher(date);
-        if(m.matches())
-            System.out.println("Введенное выражение является датой");
-        else if(d.matches() || k.matches())
+        String[] data = date.split("/");
+        if(m.matches()) {
+            int day = Integer.parseInt(data[0]);
+            int month = Integer.parseInt(data[1]);
+            int year = Integer.parseInt(data[2]);
+            int mistakeCounter = 0;
+            if((month == 4  || month == 6  || month == 9 || month == 11) && day == 31){
+                System.out.println("Введенное выражение не является датой");
+                mistakeCounter++;
+            }
+            if(month == 2){
+                if(day > 29){
+                    System.out.println("Введенное выражение не является датой");
+                    mistakeCounter++;
+                }
+                if(((year % 100 == 0 && year % 400 !=0) || year % 4 != 0) && day == 29){
+                    System.out.println("Введенное выражение не является датой");
+                    mistakeCounter++;
+                }
+            }
+            if(mistakeCounter == 0)
+                System.out.println("Введенное выражение является датой");
+        }else if(k.matches()) {
             System.out.println("Введенное выражение не является датой");
-        else
+        }else
             System.out.println("Введенное выражение не соответствует заданному формату даты dd/mm/yyyy");
     }
 }
